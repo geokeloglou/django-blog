a98a6eeb41d3abb972c9e5508af1671bc5e46438
George Keloglou <geokeloglou@gmail.com>
2019-03-06 18:54:11 +0200

    removed dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'George',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 18'
-    },
-    {
-        'author': 'John',
-        'title': 'Blog post 2',
-        'content': 'First second content',
-        'date_posted': 'August 27, 18'
-    },
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) 
 
