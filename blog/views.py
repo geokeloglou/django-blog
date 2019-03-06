bb595a2da81861a880d003bc92c31af767e40c70
George Keloglou <geokeloglou@gmail.com>
2019-03-06 12:48:06 +0200

    basic styling & templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'George',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 18'
+    },
+    {
+        'author': 'John',
+        'title': 'Blog post 2',
+        'content': 'First second content',
+        'date_posted': 'August 27, 18'
+    },
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) 
+
+def about(request):
+    return render(request, 'blog/about.html') 
