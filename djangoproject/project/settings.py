199bd470584cfaeebe61390d46dfa4e89a59692e
geokeloglou <geokeloglou@gmail.com>
2020-05-22 01:22:03 +0300

    Update settings.py

--- djangoproject/project/settings.py
+++ djangoproject/project/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tdq+@s=tb_t_cti%(ee@4(k!$8!uc$86*$34m3=4^#pw*jv+8o'
+# SECRET_KEY = 'tdq+@s=tb_t_cti%(ee@4(k!$8!uc$86*$34m3=4^#pw*jv+8o'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
 ALLOWED_HOSTS = ['healthspot1.herokuapp.com']
 
