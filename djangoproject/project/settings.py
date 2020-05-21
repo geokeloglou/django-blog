09ff411f47ba9830cf0f7e65e7f0eaf535c21bfb
geokeloglou <geokeloglou@gmail.com>
2020-05-21 23:02:39 +0300

    fixes

--- djangoproject/project/settings.py
+++ djangoproject/project/settings.py
@@ -127,6 +127,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
